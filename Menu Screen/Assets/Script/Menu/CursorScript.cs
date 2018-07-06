b9d95f7b4b2948b26c06680603b6cb4712958735
TSITCL\s162158 <sutrikshoto@gmail.com>
2018-07-06 10:27:15 +0900

    カーソル移動追加

---
+++ Menu Screen/Assets/Script/Menu/CursorScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour {
+
+    //Ugiのtransform
+    RectTransform UIPos;
+    Vector2 pos;
+    //初期座標
+    Vector2 initPos;
+
+	// Use this for initialization
+	void Start ()
+    {
+        UIPos = GetComponent<RectTransform>();
+        //初期座標設定
+        initPos = UIPos.anchoredPosition;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        pos = new Vector2(initPos.x, UIPos.anchoredPosition.y);
+
+        //カーソルが最上段じゃなければ
+        if (Input.GetKeyDown(KeyCode.UpArrow) && pos.y < initPos.y)
+        {
+            pos.y += 70;
+            //カーソルを上に動かす
+            GetComponent<RectTransform>().anchoredPosition = pos;
+        }
+
+        //カーソルが最下段じゃなければ
+        if (Input.GetKeyDown(KeyCode.DownArrow) && pos.y > -initPos.y)
+        {
+            pos.y -= 70;
+            //カーソルを下に動かす
+            GetComponent<RectTransform>().anchoredPosition = pos;
+        }
+	}
+}
